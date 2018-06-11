17b911df1f2958f29cfb809e91b305808915bcc0
Shahzaib Sheikh <bilalalam173@gmail.com>
2018-06-11 14:38:21 +0500

    office list data changed

--- app.js
+++ app.js
@@ -1,78 +1,107 @@
 const express = require('express')
 const app = express()
 const officeList = [{
-        name: "Random Office",
-        lastUpdated: {
-            date: "10/04/2017",
-            time: "11:37 AM",
+        Info: {
+            Name: "Random Office",
+            LastUpdated: {
+                Date: "10/04/2017",
+                Time: "11:37 AM",
+            },
         },
-        phoneNumber: "617-128-1272",
+        Phone: [{
+            "PhoneType": "200",
+            "Phone": "(123) 456-7898",
+            "PhoneExtension": "(11116878)"
+        }],
         Address: {
-            street: "67 Butler Ave.",
-            city: "Wakefield",
-            state: "MA",
-            zipCode: "21009"
+            Street: "67 Butler Ave.",
+            City: "Wakefield",
+            State: "MA",
+            ZipCode: "21009"
         },
-        email: "email@email.com",
-        users: "43"
+        Email: "email@email.com",
+        Users: "43"
     },
     {
-        name: "Random Office",
-        lastUpdated: {
-            date: "10/04/2017",
-            time: "11:37 AM",
+        Info: {
+            Name: "Random Office",
+            LastUpdated: {
+                Date: "10/04/2017",
+                Time: "11:37 AM",
+            },
         },
-        phoneNumber: "617-128-1272",
+        Phone: [{
+            "PhoneType": "200",
+            "Phone": "(123) 456-7898",
+            "PhoneExtension": "(11116878)"
+        }],
         Address: {
-            street: "67 Butler Ave.",
-            city: "Wakefield",
-            state: "MA",
-            zipCode: "21009"
+            Street: "67 Butler Ave.",
+            City: "Wakefield",
+            State: "MA",
+            ZipCode: "21009"
         },
-        email: "email@email.com",
-        users: "43"
+        Email: "email@email.com",
+        Users: "43"
     },
     {
-        name: "Random Office",
-        lastUpdated: {
-            date: "10/04/2017",
-            time: "11:37 AM",
+        Info: {
+            Name: "Random Office",
+            LastUpdated: {
+                Date: "10/04/2017",
+                Time: "11:37 AM",
+            },
         },
-        phoneNumber: "617-128-1272",
+        Phone: [{
+            "PhoneType": "200",
+            "Phone": "(123) 456-7898",
+            "PhoneExtension": "(11116878)"
+        }],
         Address: {
-            street: "67 Butler Ave.",
-            city: "Wakefield",
-            state: "MA",
-            zipCode: "21009"
+            Street: "67 Butler Ave.",
+            City: "Wakefield",
+            State: "MA",
+            ZipCode: "21009"
         },
-        email: "email@email.com",
-        users: "43"
+        Email: "email@email.com",
+        Users: "43"
     },
     {
-        name: "Random Office",
-        lastUpdated: {
-            date: "10/04/2017",
-            time: "11:37 AM",
+        Info: {
+            Name: "Random Office",
+            LastUpdated: {
+                Date: "10/04/2017",
+                Time: "11:37 AM",
+            },
         },
-        phoneNumber: "617-128-1272",
+        Phone: [{
+            "PhoneType": "200",
+            "Phone": "(123) 456-7898",
+            "PhoneExtension": "(11116878)"
+        }],
         Address: {
-            street: "67 Butler Ave.",
-            city: "Wakefield",
-            state: "MA",
-            zipCode: "21009"
+            Street: "67 Butler Ave.",
+            City: "Wakefield",
+            State: "MA",
+            ZipCode: "21009"
         },
-        email: "email@email.com",
-        users: "43"
-    }
+        Email: "email@email.com",
+        Users: "43"
+    },
+    
 ]
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Cache-Control,authorization,x-frame-options, Accept");
     res.header("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST,DELETE");
     next();
-  });
+});
 app.get('/officeList', (req, res) => {
-    res.json({Data:{Results:officeList}});
+    res.json({
+        Data: {
+            Results: officeList
+        }
+    });
 })
 
 app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port ' + process.env.PORT || 3000))
