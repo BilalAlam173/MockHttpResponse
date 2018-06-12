3bf1ee9b26617990406ad580422e9abd6635a36c
Shahzaib Sheikh <bilalalam173@gmail.com>
2018-06-12 13:29:09 +0500

    office profile cards apis

--- app.js
+++ app.js
@@ -7,7 +7,16 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST,DELETE");
     next();
 });
-app.get('/officeList', (req, res) => {
+app.get('/api/office/list', (req, res) => {
     res.json(dataService.formatResponseForList(dataService.getOfficeList()));
+})
+app.get('/api/office/detail', (req, res) => {
+    res.json(dataService.formatResponse(dataService.getOfficeDetail()));
+})
+app.get('/api/office/overview', (req, res) => {
+    res.json(dataService.formatResponse(dataService.getofficeOverview()));
+})
+app.get('/api/log/auditlogs', (req, res) => {
+    res.json(dataService.formatResponseForList(dataService.getAuditLogs()));
 })
 app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port ' + process.env.PORT || 3000))
--- dataService.js
+++ dataService.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    getOfficeList : () => {
+    getOfficeList: () => {
         return [{
                 Info: {
                     Name: "Random Office",
@@ -80,7 +80,219 @@ module.exports = {
 
         ]
     },
-    formatResponseForList : (results) => {
+    getOfficeDetail: () => {
+        return {
+            "Name": "Random Office",
+            "ImageUrl": null,
+            "Email": "email@email.com",
+            "Phone": [{
+                "Id": 21,
+                "Phone": "(123) 456-7898",
+                "PhoneType": "200",
+                "PhoneExtension": "(11116878)",
+                "Source": null
+            }],
+            Address: {
+                Street: "67 Butler Ave.",
+                City: "Wakefield",
+                State: "MA",
+                ZipCode: "21009"
+            },
+            "LastLogin": new Date(Date.UTC(96, 1, 2, 3, 4, 5)),
+            "DAT": "123455",
+            "TruckStop":"654322",
+            "ModifiedDate": new Date(Date.UTC(96, 1, 2, 3, 4, 5))
+        }
+    },
+    getofficeOverview:()=>{
+        return {
+            "AverageLoads": 0,
+            "AverageMargin": 0,
+            "TotalBid": 0,
+            "TotalLoads": 0,
+            "BidsTimeline": [
+              {
+                "Date": new Date(Date.UTC(96, 1, 2, 3, 4, 5)),
+                "Value": 0
+              },
+              {
+                "Date": new Date(Date.UTC(96, 1, 2, 3, 4, 5)),
+                "Value": 0
+              },
+              {
+                "Date": new Date(Date.UTC(96, 1, 2, 3, 4, 5)),
+                "Value": 0
+              },
+              {
+                "Date": new Date(Date.UTC(96, 1, 2, 3, 4, 5)),
+                "Value": 0
+              },
+              {
+                "Date": new Date(Date.UTC(96, 1, 2, 3, 4, 5)),
+                "Value": 0
+              },
+              {
+                "Date": new Date(Date.UTC(96, 1, 2, 3, 4, 5)),
+                "Value": 0
+              },
+              {
+                "Date": new Date(Date.UTC(96, 1, 2, 3, 4, 5)),
+                "Value": 0
+              },
+              {
+                "Date": new Date(Date.UTC(96, 1, 2, 3, 4, 5)),
+                "Value": 0
+              }
+            ],
+            "LoadsTimeline": [
+              {
+                "Date": new Date(Date.UTC(96, 1, 2, 3, 4, 5)),
+                "Value": 0
+              },
+              {
+                "Date": new Date(Date.UTC(96, 1, 2, 3, 4, 5)),
+                "Value": 0
+              },
+              {
+                "Date": new Date(Date.UTC(96, 1, 2, 3, 4, 5)),
+                "Value": 0
+              },
+              {
+                "Date": new Date(Date.UTC(96, 1, 2, 3, 4, 5)),
+                "Value": 0
+              },
+              {
+                "Date": new Date(Date.UTC(96, 1, 2, 3, 4, 5)),
+                "Value": 0
+              },
+              {
+                "Date": new Date(Date.UTC(96, 1, 2, 3, 4, 5)),
+                "Value": 0
+              },
+              {
+                "Date": new Date(Date.UTC(96, 1, 2, 3, 4, 5)),
+                "Value": 0
+              },
+              {
+                "Date": new Date(Date.UTC(96, 1, 2, 3, 4, 5)),
+                "Value": 0
+              }
+            ]
+          }
+    },
+    getAuditLogs:()=>{
+        return [
+            {
+              "Type": 1540,
+              "ActionType": 200,
+              "ClassName": "Fusion.Core.DataTransferObjects.CustomEvents.UnMarkLoadPriorityLog, Fusion.Core.DataTransferObjects, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null",
+              "Name": "UnMarkLoadPriorityLog",
+              "LoadId": 14,
+              "LoadPrimaryReference": "4360900416",
+              "OfficeName": "Random Office",
+              "UserAccountId": "e152e5d2-8de8-4fed-bb88-075f489cc9a9",
+              "UserFirstName": "Riaz",
+              "UserLastName": "Raza",
+              "References": [
+                "Office15",
+                "Load4360900416",
+                "Usere152e5d2-8de8-4fed-bb88-075f489cc9a9"
+              ],
+              "id": "580f9cc8-543a-4db1-b414-b9aa98c229d4",
+              "DateTime": new Date(Date.UTC(96, 1, 2, 3, 4, 5)),
+              "_ts": 1528719140
+            },
+            {
+              "Type": 1530,
+              "ActionType": 100,
+              "ClassName": "Fusion.Core.DataTransferObjects.CustomEvents.MarkLoadPriorityLog, Fusion.Core.DataTransferObjects, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null",
+              "Name": "MarkLoadPriorityLog",
+              "LoadId": 14,
+              "LoadPrimaryReference": "4360900416",
+              "OfficeName": "Random Office",
+              "FirstName": null,
+              "LastName": null,
+              "UserAccountId": "e152e5d2-8de8-4fed-bb88-075f489cc9a9",
+              "UserFirstName": "Riaz",
+              "UserLastName": "Raza",
+              "References": [
+                "Office15",
+                "Load4360900416",
+                "Usere152e5d2-8de8-4fed-bb88-075f489cc9a9"
+              ],
+              "id": "4bb22fc1-c64b-4207-967a-d196742fd2eb",
+              "DateTime": new Date(Date.UTC(96, 1, 2, 3, 4, 5)),
+              "_ts": 1528718224
+            },
+            {
+              "Type": 1530,
+              "ActionType": 300,
+              "ClassName": "Fusion.Core.DataTransferObjects.CustomEvents.MarkLoadPriorityLog, Fusion.Core.DataTransferObjects, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null",
+              "Name": "MarkLoadPriorityLog",
+              "LoadId": 14,
+              "LoadPrimaryReference": "4360900416",
+              "OfficeName": "Random Office",
+              "FirstName": null,
+              "LastName": null,
+              "UserAccountId": "471b5fcc-1e8d-4cfc-9c1e-90d17d12126e",
+              "UserFirstName": "mudassir",
+              "UserLastName": "dadad",
+              "References": [
+                "Office15",
+                "Load4360900416",
+                "User471b5fcc-1e8d-4cfc-9c1e-90d17d12126e"
+              ],
+              "id": "97f6578b-15bb-4707-bca2-a323337070f2",
+              "DateTime": new Date(Date.UTC(96, 1, 2, 3, 4, 5)),
+              "_ts": 1528717646
+            },
+            {
+              "Type": 1530,
+              "ActionType": 100,
+              "ClassName": "Fusion.Core.DataTransferObjects.CustomEvents.MarkLoadPriorityLog, Fusion.Core.DataTransferObjects, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null",
+              "Name": "MarkLoadPriorityLog",
+              "LoadId": 14,
+              "LoadPrimaryReference": "4360900416",
+              "OfficeName": "Random Office",
+              "FirstName": null,
+              "LastName": null,
+              "UserAccountId": "471b5fcc-1e8d-4cfc-9c1e-90d17d12126e",
+              "UserFirstName": "mudassir",
+              "UserLastName": "dadad",
+              "References": [
+                "Office15",
+                "Load4360900416",
+                "User471b5fcc-1e8d-4cfc-9c1e-90d17d12126e"
+              ],
+              "id": "3c364d94-66d1-45aa-9945-a46d296fa79b",
+              "DateTime": new Date(Date.UTC(96, 1, 2, 3, 4, 5)),
+              "_ts": 1528717589
+            },
+            {
+              "Type": 1530,
+              "ActionType": 200,
+              "ClassName": "Fusion.Core.DataTransferObjects.CustomEvents.MarkLoadPriorityLog, Fusion.Core.DataTransferObjects, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null",
+              "Name": "MarkLoadPriorityLog",
+              "LoadId": 14,
+              "LoadPrimaryReference": "4360900416",
+              "OfficeName": "Random Office",
+              "FirstName": null,
+              "LastName": null,
+              "UserAccountId": "471b5fcc-1e8d-4cfc-9c1e-90d17d12126e",
+              "UserFirstName": "mudassir",
+              "UserLastName": "dadad",
+              "References": [
+                "Office15",
+                "Load4360900416",
+                "User471b5fcc-1e8d-4cfc-9c1e-90d17d12126e"
+              ],
+              "id": "0be7c40e-21f6-4cfa-a407-9178407e01df",
+              "DateTime": new Date(Date.UTC(96, 1, 2, 3, 4, 5)),
+              "_ts": 1528717587
+            }
+          ]
+    },
+    formatResponseForList: (results) => {
         return {
             Response: {
                 Code: 200,
@@ -93,6 +305,15 @@ module.exports = {
                 TotalFilteredRecords: results.length,
                 Results: results
             }
+        }
+    },
+    formatResponse: (results) => {
+        return {
+            Response: {
+                Code: 200,
+                Message: "Success"
+            },
+            Data: results,
         }
     }
 }
